e198c033df603b88a0296d15923422123202cd9a
yamesproject <iyan10.gts@gmail.com>
2022-12-20 13:59:14 +0700

    ubah kontroller kegiatan

--- pratista/app/Http/Controllers/KegiatanController.php
+++ pratista/app/Http/Controllers/KegiatanController.php
@@ -11,9 +11,9 @@ class KegiatanController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function info_kegiatan()
     {
-        //
+        return view('kegiatan');
     }
 
     /**
